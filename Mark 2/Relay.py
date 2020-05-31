565e0de20a31bd24115651cd4eb4a14ed8a5dfb3
Sarang-R-119 <44112750+Sarang-R-119@users.noreply.github.com>
2020-05-31 15:56:19 -0400

    Check Relay with comm

---
+++ Mark 2/Relay.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+
+
+def setupRelay():
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setwarnings(False)
+	GPIO.setup(12,GPIO.OUT)
+
+def relaySwitchON():
+	    print "Relay ON"
+	    GPIO.output(12,GPIO.LOW)
+
+def relaySwitchOFF():
+	    print "Relay OFF"
+	    GPIO.output(12,GPIO.HIGH)
